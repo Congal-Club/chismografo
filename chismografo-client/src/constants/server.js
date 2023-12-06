7fdb2be9a4ac00fae4f34c818e7c033e44c5e148
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-12-06 13:07:07 -0600

    Add docker container configuration

--- chismografo-client/src/constants/server.js
+++ chismografo-client/src/constants/server.js
@@ -1,2 +1,2 @@
-export const REACT_APP_API_URL = 'http://192.168.100.14:8080/api'
-export const SERVER_URL = 'http://192.168.100.14:8080'
+export const REACT_APP_API_URL = 'http://192.168.39.17:8080/api'
+export const SERVER_URL = 'http://192.168.39.17:8080'
